7cc5eb7a9024e0fc43c085c73754ccc84354fce6
Diego García Valverde <diego@Diegos-MacBook-Air.local>
2014-12-08 18:23:40 +0100

    -Added python readline startup script to autocomplete

--- create_symblinks.sh
+++ create_symblinks.sh
@@ -2,3 +2,4 @@
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/pythonrc ~/.pythonrc
--- dotfiles.sh
+++ dotfiles.sh
@@ -1,2 +1,3 @@
 source create_symblinks.sh
 source tmux_color_fix.sh
+echo "export PYTHONSTARTUP=~/.pythonrc">>~/.bash_profile
