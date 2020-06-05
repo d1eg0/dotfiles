4a817fbdabec461a26f70cf24cdcb3d8394650cf
Diego García <dgarciava@agbar.es>
2020-06-05 13:20:01 +0200

    added aliases and bindings

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,6 @@
 source create_symblinks.sh
 source tmux_color_fix.sh
 echo "export PYTHONSTARTUP=~/.pythonrc">>~/.bash_profile
+echo "source ~/dotfiles/aliases" >>~/.zshrc
+echo "source ~/dotfiles/bindings" >>~/.zshrc
 
